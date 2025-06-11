3e3b6d644b10aab8e57318fef921eafb799c44f9
alena <mittralena@yandex.ru>
2025-06-11 11:43:35 +0500

    setup deploy

--- vite.config.ts
+++ vite.config.ts
@@ -23,5 +23,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/resume/' : '/',
+  base: './',
 });
